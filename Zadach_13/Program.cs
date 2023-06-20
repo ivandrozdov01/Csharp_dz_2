54cee0526bcd774b293cca1bd340ed3085abee78
Ivan_Drozdov <test@example.com>
2023-06-20 13:47:09 +0300

    Написал программу для решения 13 задачи

---
+++ Zadach_13/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. */
+
+int Prompt(string msg)
+{
+
+System.Console.Write($"{msg}");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число: ");
+
+int fnumber(int number)
+{
+while (number > 999)
+{
+number /= 10;
+}
+return number % 10;
+}
+
+bool check(int number)
+{
+if (number < 100)
+return false;
+else return true;
+}
+
+if (check(number) != true)
+System.Console.WriteLine("Третьей цифры нет");
+else
+System.Console.WriteLine($"Третья цифра числа {number} является {fnumber(number)}");
+
